434af08b6ca5c0ac4ec9cb105ef7261e188993cc
zhaosteven837 <86333176+zhaosteven837@users.noreply.github.com>
2022-04-19 14:35:47 -0400

    Adding initial gradient tests

--- ECE 2031 Project/PixelTest.asm
+++ ECE 2031 Project/PixelTest.asm
@@ -2,133 +2,23 @@
 
 ORG 0
 
-Red: LOADI 0
-	OUT PXL_CA
-	
-	OUT Timer
-T1: IN Timer
-	AND Bit1
-	JZERO T1
+LOADI 0
+OUT PXL_A
+LOAD RedStart
+OUT PXL_Red
 
+Loop1: IN PXL_Red
+	ADDI -8
+	STORE RedStore
 	LOADI 0
-	OUT PXL_A
-Red1: LOAD Red24Bit
+	OUT PXL_Blue
+	LOAD RedStore
 	OUT PXL_Red
-	LOADI 0
-	OUT PXL_Blue
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Red1
-	
-	LOADI 0
-	STORE HalfCounter
-	
-Red2: LOAD Red16Bit
-	OUT PXL_D
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Red2
-	
-	LOADI 0
-	STORE HalfCounter
-	
-P1:	IN Switches
-	AND Bit1
-	JPOS Blue
-	
-	IN Switches
-	AND Bit2
-	JPOS Green
-	
-	JUMP P1
-	
-Blue: LOADI 0
-	OUT PXL_CA
-	
-	OUT Timer
-T2: IN Timer
-	AND Bit1
-	JZERO T2
+	LOAD Counter
+	SUB End
+	JNEG Loop1
 
-	LOADI 0
-	OUT PXL_A
-Blue1: LOAD Blue24Bit
-	OUT PXL_Blue
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Blue1
-	
-	LOADI 0
-	STORE HalfCounter
-	
-Blue2: LOAD Blue16Bit
-	OUT PXL_D
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Blue2
-	
-	LOADI 0
-	STORE HalfCounter
-	
-P2:	IN Switches
-	AND Bit1
-	JZERO Red
-	
-	JUMP P2
-	
-Green: LOADI 0
-	OUT PXL_CA
-	
-	OUT Timer
-T3: IN Timer
-	AND Bit1
-	JZERO T3
 
-	LOADI 0
-	OUT PXL_A
-Green1: LOAD Green24Bit
-	OUT PXL_Green
-	LOADI 0
-	OUT PXL_Blue
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Green1
-	
-	LOADI 0
-	STORE HalfCounter
-	
-Green2: LOAD Green16Bit
-	OUT PXL_D
-	
-	LOAD HalfCounter
-	ADDI 1
-	STORE HalfCounter
-	SUB Half
-	JNEG Green2
-	
-	LOADI 0
-	STORE HalfCounter
-	
-P3:	IN Switches
-	AND Bit2
-	JZERO Red
-	
-	JUMP P3	
 	
 	
 	
@@ -138,11 +28,27 @@ Green24Bit: DW &B0000000000000001
 Green16Bit: DW &B0000000000100000
 Blue24Bit: DW  &B0000000000000001
 Blue16Bit: DW  &B0000000000000001
-HalfCounter: DW 0
-Half:      DW 4
+RedStart:  DW  &B0000000011111111
+GreenStart: DW &B0000000011111111
+BlueStart: DW  &B0000000011111111
 
-Bit1:  	   DW  &B0000000000000001
-Bit2:      DW  &B0000000000000010
+RedStore:  DW  &B0000000000000000
+GreenStore: DW &B0000000000000000
+BlueStore: DW  &B0000000000000000
+
+Counter: DW 0
+End:      DW 33
+
+Bit0:  	   DW  &B0000000000000001
+Bit1:      DW  &B0000000000000010
+Bit2:      DW  &B0000000000000100
+Bit3:      DW  &B0000000000001000
+Bit4:      DW  &B0000000000010000
+Bit5:      DW  &B0000000000100000
+Bit6:      DW  &B0000000001000000
+Bit7:      DW  &B0000000010000000
+Bit8:      DW  &B0000000100000000
+Bit9:      DW  &B0000001000000000
 	
 ; IO address constants
 Switches:  EQU 000
